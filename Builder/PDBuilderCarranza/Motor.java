80adb048c6da731b81c822c66144d96038d1969d
fr3edy <alfredocarranzag5@gmail.com>
2015-10-05 16:20:40 -0700

    este es mi patron builder programado en java

---
+++ Builder/PDBuilderCarranza/Motor.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class Motor here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Motor
+{
+private int numero;
+private String potencia;
+
+public int getNumero()
+{
+    return numero;
+}
+public void setNumero(int numero)
+{
+    this.numero = numero;
+}
+public String getPotencia()
+{
+    return potencia;
+}
+public void setPotencia(String potencia)
+{
+    this.potencia = potencia;
+}
+
+}
