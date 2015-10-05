ed397caae35b9c8b003287c1838de343ea411bc1
HaoshengLin <linhaosheng1121@gmail.com>
2015-10-04 19:35:53 -0700

    PDD State ya funcionando

---
+++ State/State/src/Producto.java
@@ -0,0 +1,27 @@
+public class Producto 
+
+{ 
+
+ protected String nombre; 
+
+ 
+
+ public Producto(String nombre) 
+
+ { 
+
+ this.nombre = nombre; 
+
+ } 
+
+ 
+
+ public void visualiza() 
+
+ { 
+
+ System.out.println("Producto: " + nombre); 
+
+ } 
+
+}
